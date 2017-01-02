21cd816f5eb28cc7e8b2d524b8821dd086a4f6c0
zhanglei4reg@gmail.com <zhanglei4reg@gmail.com>
2017-01-01 19:50:52 -0500

    LIS

---
+++ algorithm/dp/LIS_LC300.java
@@ -0,0 +1,37 @@
+/**
+ * Author: Lei Zhang
+ * raymond.zhang.us@gmail.com
+ * Jan 1, 2017
+ */
+package algorithm.dp;
+
+import java.util.Arrays;
+
+public class LIS_LC300 {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int len = nums.length;
+        int dp[] = new int[len];
+        Arrays.fill(dp, 1);
+        
+        int res = 1;
+        for (int i = 1; i < len; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    res = Math.max(res, dp[i]);
+                }
+            }
+        }
+        
+        return res;
+    }
+}
