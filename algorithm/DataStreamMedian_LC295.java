4f1ad4f310b3e6c2f120591980d83e285328aa4f
zhanglei4reg@gmail.com <zhanglei4reg@gmail.com>
2017-01-14 22:20:58 -0500

    data stream median

---
+++ algorithm/DataStreamMedian_LC295.java
@@ -0,0 +1,60 @@
+/**
+ * Author: Lei Zhang
+ * raymond.zhang.us@gmail.com
+ * Jan 14, 2017
+ */
+package algorithm;
+/**
+ * See problem statement at
+ * https://leetcode.com/problems/find-median-from-data-stream/
+ * 
+ * errors happened:
+ * 1. the max heap comparator declared as a min heap
+ * 2. did not consider the relatio of incoming element and 
+ *    maxheap/minheap head.
+ * 
+ * */
+public class DataStreamMedian_LC295 {
+
+	public static void main(String[] args) {
+
+	}
+
+	
+	PriorityQueue<Integer> maxHeap = new PriorityQueue(new Comparator<Integer>(){
+        public int compare(Integer a, Integer b) {
+            return b - a;
+        }
+    });
+    PriorityQueue<Integer> minHeap = new PriorityQueue();
+
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+        if (maxHeap.size() == 0) {
+            maxHeap.offer(num);
+        } else {
+            if (num > maxHeap.peek()) {
+                minHeap.offer(num);
+            } else {
+                maxHeap.offer(num);
+            }
+            
+            if (maxHeap.size() < minHeap.size()) {
+                maxHeap.offer(minHeap.poll());
+            }
+            
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.offer(maxHeap.poll());
+            }
+        }
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return ((double) maxHeap.peek()  + (double) minHeap.peek()) / 2;
+        } else {
+            return (double) maxHeap.peek();
+        }
+    }
+}
