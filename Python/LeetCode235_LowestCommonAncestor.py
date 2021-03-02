8257f9d8d7a79cf02b32fde6dcc1611aaf9adb4e
Lei Zhang <zhanglei4reg@gmail.com>
2021-03-01 20:28:38 -0500

    Create LeetCode235_LowestCommonAncestor.py

---
+++ Python/LeetCode235_LowestCommonAncestor.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > q.val:
+            p, q = q, p
+        while root:
+            if p.val <= root.val <= q.val:
+                return root
+            if q.val < root.val:
+                root = root.left
+            else: # root.val < p.val
+                root = root.right
+        return None
