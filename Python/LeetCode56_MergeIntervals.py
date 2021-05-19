7de9d5d0c9c7c37da05838eded8b1f536d41ea61
Lei Zhang <zhanglei4reg@gmail.com>
2021-05-19 15:17:25 -0400

    Update LeetCode56_MergeIntervals.py

--- Python/LeetCode56_MergeIntervals.py
+++ Python/LeetCode56_MergeIntervals.py
@@ -1,14 +1,10 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals:
-            return []
         intervals.sort()
-        res, lo, hi = [], intervals[0][0], intervals[0][0]
+        res = []
         for start, end in intervals:
-            if hi < start:
-                res.append([lo, hi])
-                lo, hi = start, end
+            if not res or res[-1][1] < start:
+                res.append([start, end])
             else:
-                hi = max(hi, end)
-        res.append([lo, hi])
+                res[-1][1] = max(res[-1][1], end)
         return res
