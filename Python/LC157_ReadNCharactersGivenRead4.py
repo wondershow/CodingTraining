11988d3b315e194e2d04bf65c8e4a1bf99a95fc6
Lei Zhang <zhanglei4reg@gmail.com>
2021-05-23 09:11:06 -0400

    Create LC157_ReadNCharactersGivenRead4.py

---
+++ Python/LC157_ReadNCharactersGivenRead4.py
@@ -0,0 +1,33 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        i = 0
+        while True:
+            buf4 = [' '] * 4
+            delta = read4(buf4)
+            if delta == 0:
+                break
+            while i < n and delta > 0:
+                buf[i] = buf4.pop(0)
+                i += 1
+                delta -= 1
+        return i
