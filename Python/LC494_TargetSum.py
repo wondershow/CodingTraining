5a7550cdf1cee1195aad3efdb604e6c77a5a4387
Lei Zhang <zhanglei4reg@gmail.com>
2021-05-28 06:44:55 -0400

    Create LC494_TargetSum.py

---
+++ Python/LC494_TargetSum.py
@@ -0,0 +1,21 @@
+class Solution:
+        """
+        Intersting when the input size is 24, 2^24 will not work anymore.
+        So we have to use memo.
+        """
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        def dfs(nums, index, remain, memo):
+            if index == len(nums):
+                if remain == 0:
+                    return 1
+                return 0
+            if (index, remain) in memo:
+                return memo[(index, remain)]
+            res = 0
+            res += dfs(nums, index + 1, remain - nums[index], memo)
+            res += dfs(nums, index + 1, remain + nums[index], memo)
+            memo[(index, remain)] = res
+            return res
+            
+        
+        return dfs(nums, 0, target, {})
