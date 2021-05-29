ca2bff330777a98cff3fdaf2b291b67f66e618af
Lei Zhang <zhanglei4reg@gmail.com>
2021-05-29 19:36:43 -0400

    Update LC1428_LeftMostColumnWithAtLeastOne.py

--- Python/LC1428_LeftMostColumnWithAtLeastOne.py
+++ Python/LC1428_LeftMostColumnWithAtLeastOne.py
@@ -25,3 +25,23 @@
             return hi
         
         return -1
+    
+    """
+        Needs to practice
+        put an ant at right botom, go left if cur cell is 1, 
+        go up if cur cell is 0
+        if row outof boundary while still at last col, -1
+        else col + 1
+    """
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int: 
+        R, C = binaryMatrix.dimensions()
+        i, j = R - 1, C - 1
+        while i >=0 and j >= 0:
+            if binaryMatrix.get(i, j) == 1:
+                j -= 1
+            else:
+                i -= 1
+    
+        if j == C - 1 and i < 0:
+            return -1
+        return j + 1
