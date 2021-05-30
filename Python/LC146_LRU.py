8d31e4423039d2bf63484552d8a122f3dc586cc8
Lei Zhang <zhanglei4reg@gmail.com>
2021-05-29 20:48:40 -0400

    Update LC146_LRU.py

--- Python/LC146_LRU.py
+++ Python/LC146_LRU.py
@@ -1,56 +1,63 @@
-class DoubleLinkedNode:
+class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
         self.left = self.right = None
 
 class LRUCache:
-    "Tail in, head out"
+
     def __init__(self, capacity: int):
         self.size, self.cap = 0, capacity
-        self.head = self.tail = DoubleLinkedNode(0, 0)
-        self.head.right = self.tail
-        self.tail.left = self.head
-        self.nodes = {}
-
-    def _remove_linked_node(self, node):
-        left, right = node.left, node.right
-        left.right = right
-        right.left = left
-        del self.nodes[node.key]
+        
+        ##Add at tail, evict at head
+        self.head, self.tail = Node(0, 0), Node(0, 0)
+        self.head.right, self.tail.left = self.tail, self.head
+        
+        self.node_map = {}
+        
+    def _delete_node_dlist(self, node):
+        left_node, right_node = node.left, node.right
+        left_node.right, right_node.left = right_node, left_node
+        
+    def _insert_node_dlist(self, node):
+        old_last = self.tail.left
+        old_last.right = self.tail.left = node
+        node.left, node.right = old_last, self.tail
+    
+    # key must exist
+    def _remove_key_lru(self, key):
+        node = self.node_map[key]
+        self._delete_node_dlist(node)
         self.size -= 1
+        del self.node_map[key]
     
-    # insert a node to the right of a node
-    def _insert_after(self, left_node, node):
-        old_right = left_node.right
-        left_node.right = old_right.left = node
-        node.left, node.right = left_node, old_right
-        self.nodes[node.key] = node
+    # key must not exist
+    def _add_key_lru(self, key, val):
+        node = Node(key, val)
+        self._insert_node_dlist(node)
+        self.node_map[key] = node
         self.size += 1
-        if self.size > self.cap:
-            self._evict()
-        
-    
-    def _add_tail(self, node):
-        self._insert_after(self.tail.left, node)
-        
-    def _evict(self):
-        self._remove_linked_node(self.head.right)
-    
-    
+
     def get(self, key: int) -> int:
-        if key not in self.nodes:
+        if key not in self.node_map:
             return -1
-        node = self.nodes[key]
-        self._remove_linked_node(node)
-        self._add_tail(node)
-        return node.val
+        res = self.node_map[key].val
+        self._remove_key_lru(key)
+        self._add_key_lru(key, res)
+        return res
+        
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.nodes:
-            node = DoubleLinkedNode(key, value)
-            self._add_tail(node)
+        if key not in self.node_map:
+            self._add_key_lru(key, value)
         else:
-            node = self.nodes[key]
-            node.val = value
-            self._remove_linked_node(node)
-            self._add_tail(node)
+            self._remove_key_lru(key)
+            self._add_key_lru(key, value)
+        if self.size > self.cap:
+            self._remove_key_lru(self.head.right.key)
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
