ddcc2cf1007c713d5e22056b253645aa25b9e037
Lei Zhang <zhanglei4reg@gmail.com>
2021-05-30 13:59:25 -0400

    Update LC158_ReadNCharGivenRead4II.py

--- Python/LC158_ReadNCharGivenRead4II.py
+++ Python/LC158_ReadNCharGivenRead4II.py
@@ -1,6 +1,8 @@
 """
 Use a leftover array to hold potential unserved file contents.
 Meanwhile use an eof flag to indicate if the file seek reaches end of file
+
+The first method might be easire to understand with an EOF flag
 """
 class Solution:
     def __init__(self):
@@ -23,4 +25,24 @@ class Solution:
                 while arrival < 4:
                     self.leftover.pop()
                     arrival += 1
+        return count
+    
+    def _refill(self):
+        self.left_over = [" "] * 4
+        delta = read4(self.left_over)
+        while delta < 4:
+            self.left_over.pop()
+            delta += 1
+        
+    def read1(self, buf: List[str], n: int) -> int:
+        count = 0
+        while count < n:
+            while count < n and len(self.left_over) > 0:
+                buf[count] = self.left_over.pop(0)
+                count += 1
+            if count == n:
+                break
+            self._refill()
+            if len(self.left_over) == 0:
+                break
         return count
