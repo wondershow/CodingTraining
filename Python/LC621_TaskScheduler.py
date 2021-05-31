ac6c697de6cb943aaa94a4da817015fe06b666e3
Lei Zhang <zhanglei4reg@gmail.com>
2021-05-30 21:15:08 -0400

    Update LC621_TaskScheduler.py

--- Python/LC621_TaskScheduler.py
+++ Python/LC621_TaskScheduler.py
@@ -19,3 +19,26 @@ class Solution:
             
             timer += 1
         return timer
+    
+    """
+    Just remember the answer, no bullshit
+    https://leetcode.com/problems/task-scheduler/discuss/104500/Java-O(n)-time-O(1)-space-1-pass-no-sorting-solution-with-detailed-explanation
+    """
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = sorted(Counter(tasks).values())[::-1]
+        #print([str(a) for a in freq])
+        max_freq = freq[0]
+        max_count = 1
+        i = 1
+        while i < len(freq):
+            if freq[i] == freq[i - 1]:
+                max_count += 1
+            else:
+                break
+            i += 1
+        #print("{} {}".format(max_count, max_freq))
+        parts = max_freq - 1
+        slots_in_part = n - (max_count - 1)
+        total_slots = parts * slots_in_part
+        available_tasks = len(tasks) - max_count * max_freq
+        return len(tasks) + max(0, total_slots - available_tasks)
