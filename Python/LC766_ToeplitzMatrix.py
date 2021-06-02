d83a6bc7ea8bada77a3e193e960fc4333aff5ad6
Lei Zhang <zhanglei4reg@gmail.com>
2021-06-02 08:01:44 -0400

    Update LC766_ToeplitzMatrix.py

--- Python/LC766_ToeplitzMatrix.py
+++ Python/LC766_ToeplitzMatrix.py
@@ -6,3 +6,19 @@ class Solution:
                 if i - 1 >= 0 and j - 1 >= 0 and matrix[i - 1][j - 1] != matrix[i][j]:
                     return False
         return True
+    
+    """
+    The solution to follow up one
+    Note that in the inner loop, we have to loop from end to begining.
+    """
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        M, N = len(matrix), len(matrix[0])
+        seen = {}
+        expected = matrix[0]
+        for i in range(1, M):
+            for j in range(N - 1, -1, -1):
+                if j > 0 and matrix[i][j] != expected[j - 1]:
+                    return False
+                else:
+                    expected[j] = matrix[i][j]
+        return True
