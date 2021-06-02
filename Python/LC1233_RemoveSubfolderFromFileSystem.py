57a95c6fa492e4f59369a96c50177fe04754d799
Lei Zhang <zhanglei4reg@gmail.com>
2021-06-02 07:04:21 -0400

    Update LC1233_RemoveSubfolderFromFileSystem.py

--- Python/LC1233_RemoveSubfolderFromFileSystem.py
+++ Python/LC1233_RemoveSubfolderFromFileSystem.py
@@ -12,3 +12,19 @@ class Solution:
             if not res or not (path + "/").startswith(res[-1] + "/"):
                 res.append(path)
         return res
+    
+    """
+    1. sort all paths by length
+    2. iterate each path, check every substring ends at "/" to see if it already in the result if yes it has a parent
+    """
+    def removeSubfolders2(self, folder: List[str]) -> List[str]:
+        folder.sort(key=lambda v:len(v))
+        seen = set()
+        for path in folder:
+            size = len(path)
+            for i, c in enumerate(path):
+                if c == "/" and i != 0 and path[:i] in seen:
+                    break
+            else:
+                seen.add(path)
+        return list(seen)
