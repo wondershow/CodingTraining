3b29ce9f8b4a4e41014fb9251c059d7cb4733d7d
Lei Zhang <zhanglei4reg@gmail.com>
2021-06-02 12:08:23 -0400

    Update LC1361_ValidateBinaryTreeNodes.py

--- Python/LC1361_ValidateBinaryTreeNodes.py
+++ Python/LC1361_ValidateBinaryTreeNodes.py
@@ -1,4 +1,45 @@
+class UF:
+    def __init__(self, size):
+        self.uf = [i for i in range(size)]
+        
+    def find(self, x):
+        if self.uf[x] != x:
+            self.uf[x] = self.find(self.uf[x])
+        return self.uf[x]
+    
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if rx != ry:
+            self.uf[rx] = ry
+    
+    def get_total_components(self):
+        seen = set()
+        for i in range(len(self.uf)):
+            seen.add(self.find(i))
+        return len(seen)
+        
+        
 class Solution:
+    """
+    UF.
+    1. UF component size = 1
+    edges = n - 1
+    all indegrees = 1
+    """
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        uf = UF(n)
+        indegrees, edges = {}, 0
+        for i in range(n):
+            if leftChild[i] != -1:
+                uf.union(leftChild[i], i)
+                indegrees[leftChild[i]] = indegrees.get(leftChild[i], 0) + 1
+                edges += 1
+            if rightChild[i] != -1:
+                uf.union(rightChild[i], i)
+                indegrees[rightChild[i]] = indegrees.get(rightChild[i], 0) + 1
+                edges += 1
+        return edges == n - 1 and uf.get_total_components() == 1 and all([a == 1 for a in indegrees.values()])
+    
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
         """
         1. n - 1 edges
@@ -40,6 +81,3 @@ class Solution:
                 que.append(rightChild[node])
         #print(str(len(seen)))
         return len(seen) == n
-                 
-               
-                
