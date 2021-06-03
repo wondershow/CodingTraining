8d9969121ad125d671e0672d8672b6db08807eb2
Lei Zhang <zhanglei4reg@gmail.com>
2021-06-03 17:01:06 -0400

    Update LC724_FindPivotIndex.py

--- Python/LC724_FindPivotIndex.py
+++ Python/LC724_FindPivotIndex.py
@@ -1,5 +1,19 @@
 class Solution:
+    
+    """
+    A better solution. 
+    """
     def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        left_sum = 0
+        for i, num in enumerate(nums):
+            if left_sum == total - num - left_sum:
+                return i
+            left_sum += num
+        return -1
+    
+    
+    def pivotIndex1(self, nums: List[int]) -> int:
         N = len(nums)
         right = [0] * (N + 1)
         running_sum = 0
