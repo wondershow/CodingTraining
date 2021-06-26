7c05d3e99e7316722c04fd8d2c3102d672df3abf
Lei Zhang <zhanglei4reg@gmail.com>
2021-06-26 07:16:03 -0400

    Create LC395_LoggerRateLimiter.py

---
+++ Python/LC395_LoggerRateLimiter.py
@@ -0,0 +1,24 @@
+class Logger:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.print_timestamp = {}
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        """
+        if message not in self.print_timestamp or self.print_timestamp[message] + 10 <= timestamp:
+            self.print_timestamp[message] = timestamp
+            return True
+        return False
+        
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
