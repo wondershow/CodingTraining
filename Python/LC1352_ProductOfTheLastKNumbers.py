abf4a70ae426e5b0a5a7c336c8c72e23004fbe6d
Lei Zhang <zhanglei4reg@gmail.com>
2021-06-27 09:10:40 -0400

    Create LC1352_ProductOfTheLastKNumbers.py

---
+++ Python/LC1352_ProductOfTheLastKNumbers.py
@@ -0,0 +1,23 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.prefix = [1]
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.prefix = [1]
+        else:
+            self.prefix.append(self.prefix[-1] * num)
+
+    def getProduct(self, k: int) -> int:
+        if len(self.prefix) < k + 1:
+            return 0
+        N = len(self.prefix)
+        return self.prefix[-1] // self.prefix[N - k - 1]
+        
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
