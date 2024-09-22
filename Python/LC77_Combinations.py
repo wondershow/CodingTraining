9f97d0b7a5cc68acde37bbee50cfd06e5c75c5ca
Lei Zhang <zhanglei4reg@gmail.com>
2024-09-22 06:53:37 -0700

    Create LC77_Combinations.py

---
+++ Python/LC77_Combinations.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        def search(i, path, res):
+          """
+          Basic back tracking. At each poistion, there are 2 options, add or not add.
+          """
+            if len(path) == k:
+                res.append(path[:])
+                return
+            if i > n or len(path) + (n - i + 1) < k:
+                return
+            path.append(i)
+            search(i + 1, path, res)
+            path.pop()
+            search(i + 1, path, res)
+        
+        res, path = [], []
+        search(1, path, res)
+        return res
