a4d611ee8a349332f7697dfb23bb4662975422c7
Raymond Zhang <zhanglei4reg@gmail.com>
2024-11-03 10:37:51 -0800

    Update LC283_MoveZeros.py

--- Python/LC283_MoveZeros.py
+++ Python/LC283_MoveZeros.py
@@ -2,6 +2,16 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
+
+        invariant
+
+        1, 4, 3, 0, 0, 0, x, ....
+                 j        i
+        i points to the first unprocessed element,
+        j points to the first 0 element
+        when nums[i] is 0 do nothing and increment i by 1
+        when nums[i] is not 0, swap i, j and increment i, j by 1
         """
         i, j = 0, 0
         for i in range(len(nums)):
