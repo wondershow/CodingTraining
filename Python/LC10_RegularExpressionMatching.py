322dd9657054c28b4212d1b60d44e1d06337c4ba
Raymond Zhang <zhanglei4reg@gmail.com>
2024-11-17 20:08:46 -0800

    Update LC10_RegularExpressionMatching.py

--- Python/LC10_RegularExpressionMatching.py
+++ Python/LC10_RegularExpressionMatching.py
@@ -52,5 +52,47 @@ class Solution:
         #for line in dp:
          #   print(line)            
         return dp[M][N]
+
+def isMatch2(self, s: str, p: str) -> bool:
+        """
+        From Tushar Roy. Not very clear but seems to be simpler
+        State transtition:
+        1. s[i] == p[j] or p[j] == '.':
+            remove the current comparing chars and see if the prefix substrings match
+        2. when p[j] == '*':
+            2.1 when '*' means cancel, then match s[0...i] with p[0...j - 2]
+
+            2.2 when '*' means not cancel but repeat AND s[i] == p[j - 1]
+                then remove the last char in s[0..i] (we get s[0..i-1]) and match s[0...i-1] with p[0...j]
+                The "removing" action means remove the repeat
+        
+        """
+        M, N = len(s), len(p)
+        dp = [[False] * (N + 1) for _ in range(M + 1)]
+        dp[0][0] = True
+        for i in range(1, N, 2):
+            if p[i] == '*':
+                dp[0][i + 1] = True
+            else:
+                break 
+        for i in range(M):
+            for j in range(N):
+                if s[i] == p[j] or p[j] == '.':
+                    dp[i + 1][j + 1] = dp[i][j]
+                    continue
+                if p[j] != '*':
+                    continue
+
+                # First case, match 'a' and 'ab*', let '*' cancel the previous character
+                case1 = dp[i + 1][j - 1]
+
+                # second case 'ba' vs 'ba*', in this case we remove the trailing letter
+                # (we get 'b') and we match it with 'ba*'
+                if p[j - 1] == "." or s[i] == p[j - 1]:
+                    case1 = case1 or dp[i][j + 1]
+                dp[i + 1][j + 1] = case1
+        return dp[-1][-1]
+
+
     
     
