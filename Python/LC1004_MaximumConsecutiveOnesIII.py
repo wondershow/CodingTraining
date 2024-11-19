d908c7cdc9666c2c3512b5c7defbd5b6118ee58e
Raymond Zhang <zhanglei4reg@gmail.com>
2024-11-19 05:59:03 -0800

    Update LC1004_MaximumConsecutiveOnesIII.py

--- Python/LC1004_MaximumConsecutiveOnesIII.py
+++ Python/LC1004_MaximumConsecutiveOnesIII.py
@@ -15,3 +15,20 @@ class Solution:
             if nums[i] == 0:
                 zeros -= 1
         return res
+
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        """
+        A more readable version
+        """
+        left, right, res, quota, N = 0, 0, 0, k, len(nums)
+        for left in range(N):
+            while right < N:
+                if quota == 0 and nums[right] == 0:
+                    break
+                if nums[right] == 0:
+                    quota -= 1
+                right += 1
+            if nums[left] == 0:
+                quota += 1
+            res = max(res, right - left)
+        return res
