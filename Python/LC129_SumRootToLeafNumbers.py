da93d5aa1425e6549fa903567be1b3145d4ba266
Raymond Zhang <zhanglei4reg@gmail.com>
2024-11-19 06:12:46 -0800

    Update LC129_SumRootToLeafNumbers.py

--- Python/LC129_SumRootToLeafNumbers.py
+++ Python/LC129_SumRootToLeafNumbers.py
@@ -1,4 +1,7 @@
 class Solution:
+    """
+    This version is 2021
+    """
     def findMaxLength(self, nums: List[int]) -> int:
         seen, running, res = {0: -1}, 0, 0
         for i, num in enumerate(nums):
@@ -10,4 +13,21 @@ class Solution:
                 res = max(res, i - seen[running])
             else:
                 seen[running] = i
+        return res
+
+    """
+    This version is 2024 Nov
+    """
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        def preOrder(node, val):
+            nonlocal res
+            val = 10 * val + node.val
+            if not node.left and not node.right:
+                res += val
+            if node.left:
+                preOrder(node.left, val)
+            if node.right:
+                preOrder(node.right, val)
+        preOrder(root, 0)
         return res
