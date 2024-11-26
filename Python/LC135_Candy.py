7d321468e1e8d70c63d7ad738da298e4806c5b7b
Raymond Zhang <zhanglei4reg@gmail.com>
2024-11-25 16:44:55 -0800

    Create LC135_Candy.py

---
+++ Python/LC135_Candy.py
@@ -0,0 +1,13 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        candies = [1] * len(ratings)
+
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i + 1] + 1, candies[i])
+
+        return sum(candies)
