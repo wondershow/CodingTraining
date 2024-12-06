ef2cf36f732b5972ccdf49b24d70312a3b52b11e
Raymond Zhang <zhanglei4reg@gmail.com>
2024-12-06 13:54:13 -0800

    Update LC1539_KthMissingPositiveNumber.py

--- Python/LC1539_KthMissingPositiveNumber.py
+++ Python/LC1539_KthMissingPositiveNumber.py
@@ -10,10 +10,15 @@ class Solution:
         return lo + k
     
     """
-    define f(i) = arr[i] - (i + 1)
-    The key is to find the index where f(i) < k and f(i + 1) >= k
-    say the answer is M, which means f(M) < k, f(M + 1) >=k
-    that means all k numbers after M will be missing, so M + K
+    Define: S (1,2,3,4,...M), we have (M - k) elements in the arr, then M is the kth missing element.
+
+    Define: f(i) = arr[i] - (i + 1)
+    Explaination: f(i) is the number of missing numbers that arr[:(i + 1)] has
+
+    1. when f(i) is smaller than k, that means to find S, we have to increase i to the right of the arr.
+    2. when f(i - 1) < k and f(i) >= k meaning, arr[:(i + 1)] misses equal or more than k numbers. 
+        Therefore we have 1,2,3,4,..M,..., arr[i] (note that arr[i] might be equal to M)
+        Since we are confident that arr[0...(i - 1)] (all together i numbers) present in our (1,2,...M) sequence, note that M is the Kth missing number. So we can say i + k = M so we find M.
     """
     def findKthPositive(self, arr: List[int], k: int) -> int:
         lo, hi = 0, len(arr) - 1
