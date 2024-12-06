e5ad1f5d37a133c9fee71c08b2a26fa5a329a80e
Raymond Zhang <zhanglei4reg@gmail.com>
2024-12-06 09:18:41 -0800

    Update LC708_InsertIntoASortedCircularLinkedList.py

--- Python/LC708_InsertIntoASortedCircularLinkedList.py
+++ Python/LC708_InsertIntoASortedCircularLinkedList.py
@@ -38,3 +38,41 @@ class Solution:
         pre.next = new_node
         new_node.next = cur
         return head
+
+
+    def insert2(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        newNode = Node(insertVal)
+        # 1. When it is empty
+        if not head:
+            newNode.next = newNode
+            return newNode
+
+        # 2. When there is only 1 node
+        if head.next == head:
+            head.next, newNode.next = newNode, head
+            return head
+
+
+        # 3. Find the smallest node and pre
+        cur, pre = head, None
+        while cur:
+            # When the cur val is smaller than pre, meanning we hit the last element of the sorted list
+            # Note the exit condition should be at the very end.
+            if pre and cur.val < pre.val:
+                break
+            cur, pre = cur.next, cur
+            if cur == head:
+                break
+        
+        # 4. if the insertval smaller than the smallest or larger than pre  insert between last and first
+        if insertVal < cur.val or insertVal > pre.val:
+            newNode.next, pre.next = cur, newNode
+            return head
+
+        # 5. if the value is between current and next, insert
+        while cur:
+            if cur.val <= insertVal <= cur.next.val:
+                newNode.next = cur.next
+                cur.next = newNode
+                break
+            cur = cur.next
