07008d9d5b7d140aab4c93a95b0dbd30bada2214
Raymond Zhang <zhanglei4reg@gmail.com>
2024-12-06 10:06:07 -0800

    Create LC48_RotateImage.py

---
+++ Python/LC48_RotateImage.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        def transpose(matrix):
+            N = len(matrix)
+            for i in range(N):
+                for j in range(i):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        def reflectRows(matrix):
+            N = len(matrix)
+            for i in range(N):
+                for j in range(N // 2):
+                    matrix[i][j], matrix[i][N - 1 - j] = matrix[i][N - 1 - j], matrix[i][j]
+
+        transpose(matrix)
+        reflectRows(matrix)
