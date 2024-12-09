dc1599647c160970d4cb14ab90f28f1a5811b870
Raymond Zhang <zhanglei4reg@gmail.com>
2024-12-09 15:14:01 -0800

    Update LC236_LCA_BinaryTree.py

--- Python/LC236_LCA_BinaryTree.py
+++ Python/LC236_LCA_BinaryTree.py
@@ -44,3 +44,11 @@ class Solution:
 
         return res
 
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root in [p, q]:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left if left else right
