2cdc7b4c6af6d04c0fed8d290fc5c9cbf5b73633
Raymond Zhang <zhanglei4reg@gmail.com>
2024-12-08 19:56:32 -0800

    Update LC68_TextJustification.py

--- Python/LC68_TextJustification.py
+++ Python/LC68_TextJustification.py
@@ -28,6 +28,28 @@ class Solution:
                 res.append(format_line(words[i:j], maxWidth))
             else:
                 last_line = " ".join(words[i:j])
+
+    def fullJustify1(self, words: List[str], maxWidth: int) -> List[str]:
+        """
+        This one is much concise and elegant.
+        """
+        currentLine, numLetters, result = [], 0, []
+        for word in words:
+            if len(currentLine) + len(word) + numLetters > maxWidth:
+                if len(currentLine) == 1:
+                    currentLine[0] += " " * (maxWidth - len(currentLine[0]))
+                else:
+                    for i in range(maxWidth - numLetters):
+                        currentLine[i % (len(currentLine) - 1)] += " "
+                result.append("".join(currentLine))
+                currentLine, numLetters = [], 0
+            currentLine.append(word)
+            numLetters += len(word)
+
+        lastLine = " ".join(currentLine)
+        lastLine += " " * (maxWidth - len(lastLine))
+        result.append(lastLine)
+        return result
                 last_line += " " * (maxWidth - len(last_line))
                 res.append(last_line)
             i = j
