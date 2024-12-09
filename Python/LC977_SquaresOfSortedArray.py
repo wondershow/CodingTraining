6b468323f3601584a5badeb0bd8c0e2d7aa168cf
Raymond Zhang <zhanglei4reg@gmail.com>
2024-12-09 04:56:21 -0800

    Update LC977_SquaresOfSortedArray.py

--- Python/LC977_SquaresOfSortedArray.py
+++ Python/LC977_SquaresOfSortedArray.py
@@ -1,21 +1,13 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        lo, hi = 0, len(nums) - 1
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if nums[mid] >= 0:
-                hi = mid
+        def sortedSquares(self, nums: List[int]) -> List[int]:
+        l, r = 0, len(nums) - 1
+        res, i = [0] * len(nums), len(nums) - 1
+        while l <= r:
+            if abs(nums[l]) < abs(nums[r]):
+                res[i] = abs(nums[r]) * abs(nums[r])
+                i, r = i - 1, r - 1
             else:
-                lo = mid + 1
-        l, r = lo - 1, lo
-        res = []
-        while l >= 0 or r < len(nums):
-            l_val = float("inf") if l < 0 else nums[l]
-            r_val = float("inf") if r == len(nums) else nums[r]
-            if abs(l_val) < abs(r_val):
-                res.append(l_val * l_val)
-                l -= 1
-            else:
-                res.append(r_val * r_val)
-                r += 1
+                res[i] = abs(nums[l]) * abs(nums[l])
+                i, l = i - 1, l + 1
         return res
