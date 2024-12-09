ae0462c6d3c32b86fd3331ae27e424544e447d23
Raymond Zhang <zhanglei4reg@gmail.com>
2024-12-08 18:50:19 -0800

    Update LC8_StringtoInteger_Atoi.py

--- Python/LC8_StringtoInteger_Atoi.py
+++ Python/LC8_StringtoInteger_Atoi.py
@@ -2,7 +2,7 @@ class Solution:
     """
     Just follow the statements.
     """
-    def myAtoi(self, s: str) -> int:
+    def myAtoi1(self, s: str) -> int:
         val, index, N = 0, 0, len(s)
         factor = 1
         while index < N and s[index] == ' ':
@@ -25,3 +25,25 @@ class Solution:
         if val >= 2**31:
             val = 2**31 - 1
         return val
+
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        if not s:
+            return 0
+        val, sign, i = 0, 1, 0
+        if s[0] in "+-":
+            sign = -1 if s[0] == "-" else 1
+            i += 1
+
+        while i < len(s) and s[i].isdigit():
+            val = val * 10 + int(s[i])
+            i += 1
+
+        val = val * sign
+
+        if val < -2**31:
+            val = -2**31
+        if val > 2**31 - 1:
+            val = 2**31 - 1
+        return val
+
