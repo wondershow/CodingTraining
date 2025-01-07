a49d86338dff58021696fea100e1966acd2438da
Raymond Zhang <zhanglei4reg@gmail.com>
2025-01-06 16:29:47 -0800

    Update LC1344_AngleBetweenHandsOfAClock.py

--- Python/LC1344_AngleBetweenHandsOfAClock.py
+++ Python/LC1344_AngleBetweenHandsOfAClock.py
@@ -2,12 +2,12 @@ class Solution:
         """
         Basic math
         """
-    def angleClock(self, hour: int, minutes: int) -> float:
-        offset_hour_angle = 360 * (minutes + (hour % 12)  * 60) / 720
-        offset_minutes_angle = 360 * minutes / 60
-        
-        res = (offset_hour_angle - offset_minutes_angle + 360) % 360
-        
-        if res > 180:
-            res = 180 - (res % 180)
-        return res
+        def angleClock(self, hour: int, minutes: int) -> float:
+                offset_hour_angle = 360 * (minutes + (hour % 12)  * 60) / 720
+                offset_minutes_angle = 360 * minutes / 60
+                
+                res = (offset_hour_angle - offset_minutes_angle + 360) % 360
+                
+                if res > 180:
+                    res = 180 - (res % 180)
+                return res
