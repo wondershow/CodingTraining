5e0b851c310dec4a0a1fc2b87af866b017fd2166
Raymond Zhang <zhanglei4reg@gmail.com>
2025-01-06 16:27:44 -0800

    Update LC68_TextJustification.py

--- Python/LC68_TextJustification.py
+++ Python/LC68_TextJustification.py
@@ -1,36 +1,4 @@
 class Solution:
-    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        def format_line(arr, maxWidth):
-            spaces = maxWidth - sum([len(a) for a in arr])
-            if len(arr) == 1:
-                return arr[0] + " " * spaces
-            num_of_spaces = spaces // (len(arr) - 1)
-            surplus = spaces % (len(arr) - 1)
-            res = ""
-            for i, word in enumerate(arr):
-                res += word
-                if i < len(arr) - 1:
-                    res += " " * num_of_spaces
-                    if i < surplus:
-                        res += " "
-            return res
-        
-        i, N = 0, len(words)
-        res = []
-        while i < N:
-            size, j = 0, i
-            while j < N:
-                if size + len(words[j]) > maxWidth:
-                    break
-                size += len(words[j]) + 1
-                j += 1
-            if j < N:
-                res.append(format_line(words[i:j], maxWidth))
-            else:
-                last_line = " ".join(words[i:j])
-        i = j
-        return res
-
     def fullJustify1(self, words: List[str], maxWidth: int) -> List[str]:
         """
         This one is much concise and elegant.
