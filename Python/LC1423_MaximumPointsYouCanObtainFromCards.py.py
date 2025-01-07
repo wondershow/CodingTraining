05e4ed04f4443c012012cf1f8eec46f4f1a71da3
Raymond Zhang <zhanglei4reg@gmail.com>
2025-01-06 16:24:37 -0800

    Update LC1423_MaximumPointsYouCanObtainFromCards.py

--- Python/LC1423_MaximumPointsYouCanObtainFromCards.py.py
+++ Python/LC1423_MaximumPointsYouCanObtainFromCards.py.py
@@ -1,10 +1,10 @@
   class Solution:
-    def maxScore(self, cardPoints: List[int], k: int) -> int:
-        N = len(cardPoints)
-        k = N - k
-        total = min_sum = sum(cardPoints[:k])
-        for i in range(k, N):
-            total -= cardPoints[i - k]
-            total += cardPoints[i]
-            min_sum = min(min_sum, total)
-        return sum(cardPoints) - min_sum
+      def maxScore(self, cardPoints: List[int], k: int) -> int:
+          N = len(cardPoints)
+          k = N - k
+          total = min_sum = sum(cardPoints[:k])
+          for i in range(k, N):
+              total -= cardPoints[i - k]
+              total += cardPoints[i]
+              min_sum = min(min_sum, total)
+          return sum(cardPoints) - min_sum
